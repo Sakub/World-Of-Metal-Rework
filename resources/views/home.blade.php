6e73a4770313913aca61c9745681f4f3f599edd1
Sakub <sandomierzj@gmail.com>
2020-12-29 20:43:36 +0100

    Create basic home view
    Apply bootstrap
    Divide webpage into partials like footer, header etc.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="text-center">
+            <p class="display-4 text-primary">Hello world</p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+    <div class="fixed-bottom bg-primary text-white p-2">
+        Copyright &copy; 2020/2021
+    </div>
+</body>
+</html>
