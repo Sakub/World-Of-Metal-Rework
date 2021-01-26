5ee46552f70179c70ef1f055630b1669c1056fbf
Sakub <sandomierzj@gmail.com>
2021-01-26 18:43:07 +0100

    Create searchbar in artists view

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 const topArtistsWrapper = document.querySelector('.topArtists__bands');
+const searchbar = document.querySelector('.filters__searchbar');
 
+let searchValue = '';
 let topArtists = document.querySelectorAll('.topArtists__band');
 let topArtistsNumber = topArtists.length;
 
@@ -10,4 +12,30 @@ if (topArtistsNumber == 7) {
     topArtists.forEach(topArtist => {
         topArtist.classList.remove('mx-2');
     });
+}
+
+searchbar.addEventListener('keydown', event => {
+    if (event.key === "Backspace") {
+        searchValue = searchValue.slice(0, -1);
+    } else if (event.key === "Tab" || event.key === "Enter") {
+        return;
+    } else {
+        searchValue += event.key;
+    }
+    search(searchValue);
+});
+
+function search(fraze) {
+    let items;
+    if (window.location.pathname == '/bands')  items = document.querySelectorAll('.band');
+    if (window.location.pathname == '/albums') items = document.querySelectorAll('.album');
+    
+    for(let i=0; i<items.length; i++) {
+        let itemName = items[i].getAttribute('data-name').toLowerCase();
+        if (itemName.indexOf(fraze) > -1) {
+            items[i].style.display = '';
+        } else {
+            items[i].style.display = 'none';
+        }
+    }
 }
--- resources/views/bands/index.blade.php
+++ resources/views/bands/index.blade.php
@@ -1,33 +1,29 @@
 @extends('layouts.app')
 @section('content')
-<div class="content mx-auto bands__content">
-    <div class="row justify-content-between">
-        <p class="section__heading text-custom-primary font-weight-bold">Filters:</p>
-        <p class="section__viewMore text-white font-weight-bold">Artists</p>
-    </div>
-    <div class="row">
-        <div class="content__filters">
-
+    <div class="content mx-auto bands__content">
+        <div class="row justify-content-between">
+            <p class="section__heading text-custom-primary font-weight-bold">Filters:</p>
+            <p class="section__viewMore text-white font-weight-bold">Artists</p>
         </div>
-        <div class="content__bands">
-            <div class="row justify-content-end">
-                @foreach($bands as $band)
-                    <div class="band">
-                        <a href="{{route('bands.show', $band->id)}}">
-                            <img src="{{asset($band->image_url)}}" alt="{{$band->name}}" class="band__image">
-                            <div class="band__overlay text-white">
-                                <div class="band__name">{{$band->name}}</div>
-                                <div class="band__rating font-weight-bold">{{$band->rating}} <i class="bi bi-star-fill"></i>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
+        <div class="row">
+            <div class="content__filters">
+                <input type="text" placeholder="Search..." class="filters__searchbar text-custom-primary bg-transparent rounded-pill">
             </div>
-
+            <div class="content__bands">
+                <div class="row justify-content-end">
+                    @foreach($bands as $band)
+                        <div class="band" data-name="{{$band->name}}">
+                            <a href="{{route('bands.show', $band->id)}}">
+                                <img src="{{asset($band->image_url)}}" alt="{{$band->name}}" class="band__image">
+                                <div class="band__overlay text-white">
+                                    <div class="band__name">{{$band->name}}</div>
+                                    <div class="band__rating font-weight-bold">{{$band->rating}} <i class="bi bi-star-fill"></i></div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
-
-</div>
-
 @endsection
